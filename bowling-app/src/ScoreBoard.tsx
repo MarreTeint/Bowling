5ac97b9e51789de75ff8eef95e336d7ef927300d
death-joke <leborgnekevin430@gmail.com>
2023-01-04 16:37:10 +0100

    ScoringFinalBoard

--- bowling-app/src/ScoreBoard.tsx
+++ bowling-app/src/ScoreBoard.tsx
@@ -1,11 +1,45 @@
 import React from "react";
-
 export default function ScoreBoards(props: any) {
     const [playerList, setPlayerList] = React.useState(props.playerList);
     const [scoreList, setScoreList] = React.useState(props.scoreList);
+
+
     return (
         <div className="scoreboard">
-            Notre tableau de scores 
+            <table>
+        <thead>
+            <td>Player </td>
+          <td colSpan={21}> Score Board</td>
+          <td>Total </td>
+       
+    </thead>
+    <tbody>
+        <PLayerSCoreBoards PlayerName={playerList[0]} PlayerSCore={scoreList[0]} />
+        <PLayerSCoreBoards PlayerName={playerList[1]} PlayerSCore={scoreList[1]} />
+   
+
+    </tbody>
+</table>
+            
        </div>
     );    
+}
+
+function PLayerSCoreBoards(props: any) {
+    const PlayerSCore = props.PlayerSCore;
+    const PlayerName = props.PlayerName;
+
+    //return un tableau des scores
+    const Score = PlayerSCore.map((score: any) =>
+        <td>{score}</td>
+    );
+
+    return (
+        <tr className="scoreboard">
+            <td scope="row">{PlayerName}</td>
+            {Score}
+            <td>No</td>
+        </tr>
+    );   
+    
 }
