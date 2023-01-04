a575e6acac70f44ebc7184a680b2cf6e2609e32f
Martin SEBIROT <96467585+MarreTeint@users.noreply.github.com>
2023-01-04 17:30:47 +0100

    a revoir

--- bowling-app/src/ScoreManager.tsx
+++ bowling-app/src/ScoreManager.tsx
@@ -3,6 +3,21 @@ import React from "react";
 export default function ScoreManager(props: any) {
     const [playerList, setPlayerList] = React.useState(props.playerList);
     const [scoreList, setScoreList] = React.useState(props.scoreList);
+    const updateScore = (player: number, lance: number, score: number) => {
+        var newScoreList = scoreList;
+        if(score == 10){
+            newScoreList[player][lance] = 'X';
+            if(lance < 18){
+                setLance(1);
+            }
+        }else if(score + scoreList[player][lance-1] == 10){
+            newScoreList[player][lance] = '/';
+        }else{
+            newScoreList[player][lance] = score;
+        }
+        setScoreList(newScoreList);
+    }
+
 
     const[round, setRound] = React.useState(1);
     const [lance, setLance] = React.useState(0);
@@ -25,22 +40,35 @@ export default function ScoreManager(props: any) {
                 <option value = "10">10</option>
             </select>
             <button onClick={()=>{
-                if(lance+1 == 2 && round < 10){
-                    setPlayer((player+1)%2);
-                } 
-                else if(lance+1 == 3 && round == 10){
-                    setPlayer((player+1)%2);
-                }
-                if(round == 10){
-                    setLance((lance+1)%3);
+                var score = document.getElementById("quilles") as HTMLSelectElement;
+                var scoreValue = parseInt(score.value);
+                if(round < 10 && lance == 1 && scoreValue + scoreList[player][Math.pow(2, round-1)] <= 10 || round == 10 || lance == 0){
+                    var numlance = Math.pow(2, round-1)+lance;
+                    if(round == 1){
+                        numlance = lance;
+                    }
+                    updateScore(player, numlance, scoreValue);
+
+                    if(lance+1 == 2 && round < 10){
+                        setPlayer((player+1)%2);
+                    } 
+                    else if(lance+1 == 3 && round == 10){
+                        setPlayer((player+1)%2);
+                    }
+                    if(round == 10 && lance == 1 && (scoreList[player][Math.pow(2, round-1)] == 'X' || scoreList[player][Math.pow(2, round-1)+1] == '/')){
+                        setLance(2);
+                    }else{
+                        setLance((lance+1)%2);
+                    }
+                    if(player == 1 && lance == 1 && round < 10){
+                        setRound(round+1);
+                    }else if(player == 1 && lance == 2 && round == 10){
+                        setRound(round+1);
+                    }
                 }else{
-                    setLance((lance+1)%2);
+                    alert("Score invalide")
                 }
-                if(player == 1 && lance == 1 && round < 10){
-                    setRound(round+1);
-                }else if(player == 1 && lance == 2 && round == 10){
-                    setRound(round+1);
-                }
+                console.log(scoreList);
             }
             }>Valider</button>      
        </div>
--- bowling-app/src/build/ScoreManager.js
+++ bowling-app/src/build/ScoreManager.js
@@ -16,24 +16,56 @@ var __assign = (this && this.__assign) || function () {
 export default function ScoreManager(props) {
     var _a = React.useState(props.playerList), playerList = _a[0], setPlayerList = _a[1];
     var _b = React.useState(props.scoreList), scoreList = _b[0], setScoreList = _b[1];
+    var updateScore = function (player, lance, score) {
+        var newScoreList = scoreList;
+        if (score == 10) {
+            newScoreList[player][lance] = 'X';
+            if (lance < 18) {
+                setLance(1);
+            }
+        }
+        else if (score + scoreList[player][lance - 1] == 10) {
+            newScoreList[player][lance] = '/';
+        }
+        else {
+            newScoreList[player][lance] = score;
+        }
+        setScoreList(newScoreList);
+    };
     var _c = React.useState(1), round = _c[0], setRound = _c[1];
     var _d = React.useState(0), lance = _d[0], setLance = _d[1];
     var _e = React.useState(0), player = _e[0], setPlayer = _e[1];
     return (_jsxs("div", __assign({ className: "scoreManager" }, { children: ["Au tour de ", playerList[player], _jsx("br", {}), "Tour n\u00B0", round, " | Lance n\u00B0", lance + 1, _jsx("br", {}), _jsxs("select", __assign({ id: "quilles" }, { children: [_jsx("option", __assign({ value: "0" }, { children: "0" })), _jsx("option", __assign({ value: "1" }, { children: "1" })), _jsx("option", __assign({ value: "2" }, { children: "2" })), _jsx("option", __assign({ value: "3" }, { children: "3" })), _jsx("option", __assign({ value: "4" }, { children: "4" })), _jsx("option", __assign({ value: "5" }, { children: "5" })), _jsx("option", __assign({ value: "6" }, { children: "6" })), _jsx("option", __assign({ value: "7" }, { children: "7" })), _jsx("option", __assign({ value: "8" }, { children: "8" })), _jsx("option", __assign({ value: "9" }, { children: "9" })), _jsx("option", __assign({ value: "10" }, { children: "10" }))] })), _jsx("button", __assign({ onClick: function () {
-                    if (lance + 1 == 2 && round < 10) {
-                        setPlayer((player + 1) % 2);
-                    }
-                    else if (lance + 1 == 3 && round == 10) {
-                        setPlayer((player + 1) % 2);
-                    }
-                    if (round == 10) {
-                        setLance((lance + 1) % 3);
+                    var score = document.getElementById("quilles");
+                    var scoreValue = parseInt(score.value);
+                    if (round < 10 && lance == 1 && scoreValue + scoreList[player][Math.pow(2, round - 1)] <= 10 || round == 10 || lance == 0) {
+                        var numlance = Math.pow(2, round - 1) + lance;
+                        if (round == 1) {
+                            numlance = lance;
+                        }
+                        updateScore(player, numlance, scoreValue);
+                        if (lance + 1 == 2 && round < 10) {
+                            setPlayer((player + 1) % 2);
+                        }
+                        else if (lance + 1 == 3 && round == 10) {
+                            setPlayer((player + 1) % 2);
+                        }
+                        if (round == 10 && lance == 1 && (scoreList[player][Math.pow(2, round - 1)] == 'X' || scoreList[player][Math.pow(2, round - 1) + 1] == '/')) {
+                            setLance(2);
+                        }
+                        else {
+                            setLance((lance + 1) % 2);
+                        }
+                        if (player == 1 && lance == 1 && round < 10) {
+                            setRound(round + 1);
+                        }
+                        else if (player == 1 && lance == 2 && round == 10) {
+                            setRound(round + 1);
+                        }
                     }
                     else {
-                        setLance((lance + 1) % 2);
+                        alert("Score invalide");
                     }
-                    if (player == 1 && lance == 1) {
-                        setRound(round + 1);
-                    }
+                    console.log(scoreList);
                 } }, { children: "Valider" }))] })));
 }
