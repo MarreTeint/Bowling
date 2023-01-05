afd8a87036acba271e4dd928fd4b3ade16d474c1
death-joke <leborgnekevin430@gmail.com>
2023-01-05 09:15:06 +0100

    fix-import

--- bowling-app/src/build/index.js
+++ bowling-app/src/build/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-var root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(_jsx(React.StrictMode, { children: _jsx(App, {}) }));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
