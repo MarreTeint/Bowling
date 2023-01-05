9aba0b4fdadca16adcd5ae3078ce75eab01d58ad
death-joke <leborgnekevin430@gmail.com>
2023-01-05 09:51:05 +0100

    fix-import

--- bowling-app/src/build/App.js
+++ bowling-app/src/build/App.js
@@ -14,9 +14,11 @@ import './App.css';
 import ScoreBoard from "./build/ScoreBoard";
 import ScoreManager from "./build/ScoreManager";
 var pLayerList = ["Player 1", "Player 2"];
+//const scoreList = ;
+//array de 21 space par joueur
 var scoreList = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 ];
 function App() {
     return (_jsxs("div", __assign({ className: "App" }, { children: [_jsx(ScoreBoard, { playerList: pLayerList, scoreList: scoreList }), _jsx(ScoreManager, { playerList: pLayerList, scoreList: scoreList })] })));
--- bowling-app/src/build/ScoreBoard.js
+++ bowling-app/src/build/ScoreBoard.js
@@ -1,16 +1,7 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import React from "react";
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
+
+
 
 export default function ScoreBoards(props) {
     var _a = React.useState(props.playerList), playerList = _a[0], setPlayerList = _a[1];
