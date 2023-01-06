15dbc54ffd2b19f34ff023e2ccf1a4c88434c109
Mitrano-sensei <56512461+Mitrano-sensei@users.noreply.github.com>
2023-01-06 15:06:37 +0100

    score functions

--- bowling-app/src/App.tsx
+++ bowling-app/src/App.tsx
@@ -5,22 +5,55 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
+
+/**
+ * Returns the score array from a rolls array
+ * 
+ * @remarks
+ * /!\ A strike is represented by a [10], not a [10, 0] /!\
+ * 
+ * @param rolls - array representing the succesive rolls of a bowling game 
+ * 
+ * @returns
+ * The score array
+ */
+function scoreFromPins(pins:number[]) {
+  var score:number[] = new Array(10);
+  var counter:number = 0;
+  for (let i = 0; i + 1 < pins.length; i+=2)
+  {
+    if (pins[i] + pins[i+1] < 10)
+    {
+      score[counter] = pins[i] + pins[i+1];
+      counter++;
+      continue;
+    }
+
+    if (i+2 >= pins.length)
+    {
+      break;
+    }
+
+    score[counter] = pins[i] + pins[i+1] + pins[i+2]
+    counter+=1
+
+    if (pins[i] === 10)
+    {
+      i--;
+    }
+
+  }
+
+  for (let i = 1; i < 10; i++)
+  {
+    score[i] += score[i-1];
+  }
+
+  return score;
+}
+
 
 export default App;
