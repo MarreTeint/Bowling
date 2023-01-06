342d20c456d0710c8f942f0b8a8cb8a31b02720c
kevin <death-joke>
2023-01-06 17:06:20 +0100

    score_converter

--- bowling-app/src/App.tsx
+++ bowling-app/src/App.tsx
@@ -15,30 +15,13 @@ const scoreList = [
 
 function App() {
 
-  const rolls1:number[] = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
-  const expected_score1:number[] = [30, 60, 90, 120, 150, 180, 210, 240, 300];
 
-  const rolls2:number[] = [8, 1, 0, 9, 2, 8, 10, 6, 3, 7, 0, 5, 2, 10, 0, 6, 2, 8, 10];
-  const expected_score2:number[] = [9, 18, 38, 57, 66, 73, 80, 96, 102, 122];
-
-  var r1 = scoreFromPins(rolls1);
-  var r2 = scoreFromPins(rolls2);
-
-  var str1:string = "";
-  var str2:string = "";
-
-  for (let i = 0; i < 10; i++)
-  {
-    str1 += r1[i]+" ";
-    str2 += r2[i]+" ";
-  }
 
   return (
     <div className="App">
     {// TODO : SORTIR LE SCOREBOARD DU SCOREMANAGER
   }
-    {str1} <br></br>
-    {str2}
+    
       <ScoreManager playerList={pLayerList} scoreList={scoreList} />
      
     
