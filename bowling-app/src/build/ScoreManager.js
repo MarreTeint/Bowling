07dc7a42491ea764f3d1837c9e4ceb174c23f67b
Martin SEBIROT <96467585+MarreTeint@users.noreply.github.com>
2023-01-06 14:10:37 +0100

    add: round number

--- bowling-app/src/build/ScoreManager.js
+++ bowling-app/src/build/ScoreManager.js
@@ -1,7 +1,7 @@
 import { jsx as _jsx } from "react/jsx-runtime";
 import React from "react";
 export default function ScoreManager(props) {
-    var _a = React.useState(props.playerList), playerList = _a[0], setPlayerList = _a[1];
-    var _b = React.useState(props.scoreList), scoreList = _b[0], setScoreList = _b[1];
+    const [playerList, setPlayerList] = React.useState(props.playerList);
+    const [scoreList, setScoreList] = React.useState(props.scoreList);
     return (_jsx("div", { className: "scoreboard" }));
 }
