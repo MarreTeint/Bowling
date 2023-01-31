efde3f2c788275802665973d6f969c21e800b7ab
kevin <death-joke>
2023-01-31 16:25:28 +0100

    -amend

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,24 @@ import ScoreManager from "./scoreManager";
 import {scoreListContext} from "./context/scorelist";
 import {playerListContext} from "./context/playerlist";
 import {roundContext} from "./context/round";
+import {roundNumberContext} from "./context/roundNumber";
+import {quilleNumberContext} from "./context/quilleNumber";
+
 import './App.css';
 
 
 function App() {
-  const [scoreList, setScoreList]= React.useState([[" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],[" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "]]);
-   const [round, setRound]= React.useState(1);
-  const [playerList, setPlayerList]= React.useState(["Player 1", "Player 2"]);
+    const [scoreList, setScoreList]= React.useState([[" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],[" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "]]);
+    const [round, setRound]= React.useState(1);
+    const [quilleNumber, setquilleNumber]= React.useState(10);
+    const [roundNumber, setRoundNumber]= React.useState(10);
+    const [playerList, setPlayerList]= React.useState(["Player 1", "Player 2"]);
+
 
     return (    
     <div className="App">
+      <roundNumberContext.Provider value={{roundNumber:roundNumber,setroundNumber:setRoundNumber}}>
+      <quilleNumberContext.Provider value={{quilleNumber:quilleNumber,setquilleNumber:setquilleNumber}}>
       <roundContext.Provider value={{round:round,setRound:setRound}}>
         <playerListContext.Provider value={{playerList:playerList,setPlayerList:setPlayerList}}>
           <scoreListContext.Provider value={{scoreList: scoreList, setScoreList: setScoreList}}>
@@ -22,7 +30,9 @@ function App() {
             <ScoreManager />
           </scoreListContext.Provider>
         </playerListContext.Provider>
-      </roundContext.Provider>     
+      </roundContext.Provider>  
+      </quilleNumberContext.Provider>
+      </roundNumberContext.Provider>
     </div>
   );
 }
