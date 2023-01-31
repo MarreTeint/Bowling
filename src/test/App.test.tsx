053b090e42345e9ca309fb14ae6300706857e576
kevin <death-joke>
2023-01-31 14:21:01 +0100

    -amend

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test("test sur l'application", () => {
+test("sur l'application", () => {
   render(<App />);
   const linkElement = screen.getByText('Score Board');
   expect(linkElement).toBeInTheDocument();
