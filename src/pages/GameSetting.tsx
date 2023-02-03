8f8a21c8ded1f518c15d97369e909ce9ff8d06ed
2maxime0 <71760500+2maxime0@users.noreply.github.com>
2023-02-03 15:31:43 +0100

    Changed menu style

--- src/pages/GameSetting.tsx
+++ src/pages/GameSetting.tsx
@@ -3,7 +3,7 @@ import { StartMenu } from "../startMenu";
 const GameSetting = () => {
     return (
     <div>
-      <h1>GameSetting</h1>
+      <h1>Game Settings</h1>
       <StartMenu />
     </div>);
    
